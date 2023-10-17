0b04d0817c2ac431c14541559bddbf7f819f3fd3
AliEmreAKCAKOCA <146120490+AliEmreAKCAKOCA@users.noreply.github.com>
2023-10-17 10:42:46 +0300

    Add files via upload

---
+++ ders 19/cpp19_2/cpp19_2/cpp19_2.cpp
@@ -0,0 +1,23 @@
+﻿// cpp19_2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+
+std::ostream& dashline(std::ostream& os)
+{
+
+    return os << "\n----------------------------------------------------\n";
+}
+
+int main()
+{
+    using namespace std;
+
+    int ival{ 345 };
+    double dval{ 524.2345 };
+    long lval{ 2454l };
+
+    cout << ival << dashline << dval << dashline << lval << dashline;
+}
+
