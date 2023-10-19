b8451bbd9bec0e569246390a5411c9068fc04a61
AliEmreAKCAKOCA <146120490+AliEmreAKCAKOCA@users.noreply.github.com>
2023-10-19 22:59:22 +0300

    Add files via upload

---
+++ ders28/cpp28_3/cpp28_3/cpp28_3.cpp
@@ -0,0 +1,47 @@
+﻿// cpp28_3.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+
+int main()
+{
+
+    using namespace std;
+    std::string str{ "0123456789ABCDEFGHIJKLMNPRSTUVWYZabcdefghijklmnoprstuvwyz" };
+   
+
+    char c;
+    std::cout << "aranan karakteri girin: ";
+    std::cin >> c;
+
+
+    if (auto idx = str.find(c))  
+    {
+        std::cout << " bulundu = " << idx << "\n";
+    }
+    else
+    {
+        std::cout << "bulunamaduı\n";
+    }
+
+
+
+
+
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
