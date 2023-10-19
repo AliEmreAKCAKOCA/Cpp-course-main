94c712637d9bc353e080954b28011dc14340e926
AliEmreAKCAKOCA <146120490+AliEmreAKCAKOCA@users.noreply.github.com>
2023-10-19 22:58:21 +0300

    Add files via upload

---
+++ ders26/cpp26_4/cpp26_4/cpp26_4.cpp
@@ -0,0 +1,50 @@
+﻿// cpp26_4.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+
+// inline namesapce hakkında bu kadar bilmen yeterli dedi hhoca iileride anlatacak
+
+namespace nec {
+
+    inline namespace ver1 
+    {
+
+         class Myclass 
+         {
+
+
+
+         };
+    }
+
+
+    namespace ver2 {
+
+        class Myclass {
+
+        };
+
+    }
+
+
+}
+
+
+int main()
+{
+    nec::Myclass x;//inline olursa legal eğer 2 myclas önceliği için makro yazılabilir
+
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
