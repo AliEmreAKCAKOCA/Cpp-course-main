eaf954c4a71f6f1e8500fc9558837876537bdbae
AliEmreAKCAKOCA <146120490+AliEmreAKCAKOCA@users.noreply.github.com>
2023-11-01 21:41:32 +0300

    Add files via upload

---
+++ ders33/ders33_4/ders33_4/ders33_4.cpp
@@ -0,0 +1,46 @@
+﻿// ders33_4.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+
+class Base {
+public:
+    virtual void vfunc();
+
+};
+
+class Der :public Base {//private olsa upcasting yapamaz
+public:
+    void vfunc() override;
+
+    friend void g1(); //denirse private Base olsa bile erişebilir upcasting yapar
+
+};
+
+void foo(Base& r)
+{
+    r.vfunc();
+}
+
+
+void  g1()
+{
+    Der myder;
+    //foo(myder); // yapabilir
+}
+
+void  g2()
+{
+    Der myder;
+}
+
+void  g3()
+{
+    Der myder;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
