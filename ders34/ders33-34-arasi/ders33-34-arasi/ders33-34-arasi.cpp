1b3c91a4dd5c5cf5bf421091c3014f7de90600ff
AliEmreAKCAKOCA <146120490+AliEmreAKCAKOCA@users.noreply.github.com>
2023-11-01 21:43:00 +0300

    Add files via upload

---
+++ ders34/ders33-34-arasi/ders33-34-arasi/ders33-34-arasi.cpp
@@ -0,0 +1,95 @@
+﻿// ders33-34-arasi.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+class Device {
+public:
+    bool is_on()const {
+        return mflag;
+    }
+    void turn_on() {
+        mflag = true;
+        std::cout << "cihaz acildi\n";
+    }
+    void turn_off() {
+        mflag = false;
+        std::cout << "cihaz kapatiildi\n";
+    }
+
+private:
+    bool mflag;
+
+};
+
+class Fax :public Device {
+public:
+    virtual void send_fax() 
+    {
+        if (is_on())
+        {
+            std::cout << "fax gonderildi \n";
+        }
+        else
+        {
+            std::cout << "cihaz kapalı fax gonderilemedi \n";
+        }
+
+    }
+    virtual void receive_fax() 
+    {
+        if (is_on())
+        {
+            std::cout << "fax alindi \n";
+        }
+        else
+        {
+            std::cout << "cihaz kapalı fax alinamadi \n";
+        }
+
+    }
+
+
+};
+
+class Modem :public Device {
+public:
+    virtual void send_data()
+    {
+        if (is_on())
+        {
+            std::cout << "data gonderildi \n";
+        }
+        else
+        {
+            std::cout << "cihaz kapalı data gonderilemedi \n";
+        }
+
+    }
+
+    virtual void receive_data()
+    {
+        if (is_on())
+        {
+            std::cout << "data  alindi \n";
+        }
+        else
+        {
+            std::cout << "cihaz kapalı data alinamadi \n";
+        }
+
+    }
+};
+
+class FaxModem :public Fax, public Modem {
+
+};
+
+
+int main()
+{
+    FaxModem fm;
+    fm.Fax::turn_on();
+    fm.receive_fax();
+    fm.send_data();
+}
---
+++ ders34/ders34_1-1/ders34_1-1/ders34_1-1.cpp
@@ -0,0 +1,51 @@
+﻿// ders33_6.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//ders 33 de yazmadığın 
+
+
+#include <iostream>
+
+class x {
+
+public:
+    x(int a) {
+        std::cout << "x(int a) a= "<<a<<"\n";
+    }
+
+};
+
+class y {
+
+public:
+    y(int b) {
+        std::cout << "y(int b) b= " << b << "\n";
+    }
+
+};
+
+
+class Der : public x, public y {//suan önce x sonra y hangisi once ise o çağrılır
+
+public:
+    Der():y(1),x(9)
+    {
+        std::cout << "der default constr\n";// önce x çağırılır
+    }
+};
+
+int main()
+{
+    Der myder;
+
+    
+}
+
+// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
+// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
+
+// Kullanmaya Başlama İpuçları: 
+//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
+//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
+//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
+//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
+//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
+//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
